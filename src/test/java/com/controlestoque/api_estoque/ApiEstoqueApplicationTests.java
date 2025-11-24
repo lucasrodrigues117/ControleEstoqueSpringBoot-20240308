6cc40a584f635fbb26cdd5d44f14e3e5ac7fb645
lucasrodrigues117 <rodrigues.lds.lfr@gmail.com>
2025-11-24 17:20:27 -0300

    Adiciona os arquivos Categoria.java e Fornecedor.java

---
+++ src/test/java/com/controlestoque/api_estoque/ApiEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlestoque.api_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
